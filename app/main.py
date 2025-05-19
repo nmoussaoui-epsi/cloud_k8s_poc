f1cfae33cf3a58c2e60a923667ad894222ca0982
nmoussaoui-epsi <nassim.moussaoui@ecoles-epsi.net>
2025-05-20 01:55:37 +0200

    Add Flask app template and styling for improved UI

--- app/main.py
+++ app/main.py
@@ -1,19 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import sqlite3
 import os
 import time
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static", template_folder="templates")
 
-# Charger les variables d'env envoyées par le ConfigMap K8s
 DB_PATH = os.getenv("DB_PATH", "database.db")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 @app.route("/read_db", methods=["GET"])
 def read_db():
-    """
-    Lit tous les utilisateurs dans la table users de la BDD SQLite.
-    Retourne le résultat sous forme de JSON.
-    """
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
     try:
@@ -27,9 +26,6 @@ def read_db():
 
 @app.route("/cpu_load", methods=["GET"])
 def cpu_load():
-    """
-    Lance une boucle qui utilise le CPU pendant 3 minutes (pour tester le scaling auto).
-    """
     start_time = time.time()
     while time.time() - start_time < 180:
         _ = sum(i*i for i in range(10000))
